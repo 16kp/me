47b4b14d3dedacb385a3673dd4cdd97db1382f78
kr9s8n1 <purohitkrishnar@outlook.com>
2020-08-03 15:08:24 +0530

    Changes to markdown, body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,8 +2,7 @@ import React from 'react';
 import '../css/styles.css';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 import MailIcon from '@material-ui/icons/Mail';
 
@@ -18,10 +17,10 @@ function Body() {
                 <Grid item xs={8}>
                     <Grid container spacing={2} direction="column">
                         <Grid item xs={6}>
-                            <span className="hello">んey there @</span>
+                            <span className="hello white">んey there @</span>
                         </Grid>
                         <Grid item xs={12}>
-                            <span className="text">
+                            <span className="text white">
                                 Hello, I'm Krishna Purohit.
                             </span>
                         </Grid>
@@ -30,10 +29,12 @@ function Body() {
 
                 {/* right side containing buttons */}
                 <Grid item xs={4}>
-                    <Button variant="outlined" color="black" href="mailto:purohitkrishnar@gmail.com?subject=Hey There!" >
-                        <MailIcon />
-                        <span className="text">Hit me up!</span>
+                
+                    <Button variant="outlined" className="white-border" href="mailto:purohitkrishnar@gmail.com?subject=Hey There!" >
+                        <MailIcon className="white" />
+                        <span className="text white">Hit me up!</span>
                     </Button>
+                    
                 </Grid>
 
             </Grid>
