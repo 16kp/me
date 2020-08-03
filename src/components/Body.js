b548c765af4fb27388a8586b1634d47943f87141
kr9s8n1 <purohitkrishnar@outlook.com>
2020-08-03 12:39:46 +0530

    Adding material-ui grid, tried changing favicon

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import '../css/styles.css';
-import Fab from '@material-ui/core/Fab';
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+
 import MailIcon from '@material-ui/icons/Mail';
 
 
@@ -9,10 +12,31 @@ function Body() {
 
     return (
         <div className="App-body">
-            <Button variant="outlined" color="black">
-                <MailIcon />
-                <span className="text">Hit me up!</span>
-            </Button>
+            <Grid container spacing={2}>
+                
+                {/* left side containing info */}
+                <Grid item xs={8}>
+                    <Grid container spacing={2} direction="column">
+                        <Grid item xs={6}>
+                            <span className="hello">んey there @</span>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <span className="text">
+                                Hello, I'm Krishna Purohit.
+                            </span>
+                        </Grid>
+                    </Grid>                
+                </Grid>
+
+                {/* right side containing buttons */}
+                <Grid item xs={4}>
+                    <Button variant="outlined" color="black" href="mailto:purohitkrishnar@gmail.com?subject=Hey There!" >
+                        <MailIcon />
+                        <span className="text">Hit me up!</span>
+                    </Button>
+                </Grid>
+
+            </Grid>
         </div>
     );
 }
