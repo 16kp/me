29faf966554ce5f62d3e557e2d3c6227568254d2
kr9s8n1 <purohitkrishnar@outlook.com>
2020-08-03 15:29:38 +0530

    Updates

---
+++ precache-manifest.63f77bc734da5fd32b341a142ae9b134.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4971ae18c71666fe56af49945bd17ea9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "911977c7fb653e803742",
+    "url": "/static/css/main.f1cadb36.chunk.css"
+  },
+  {
+    "revision": "fa1a072a206ea446701d",
+    "url": "/static/js/2.c301269e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c301269e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "911977c7fb653e803742",
+    "url": "/static/js/main.ffa1a1ad.chunk.js"
+  },
+  {
+    "revision": "ddb23d41b025de166703",
+    "url": "/static/js/runtime-main.60ded422.js"
+  }
+]);
