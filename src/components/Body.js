015b221c483db97458f8a67fdd2f742c0fa3ca9b
kr9s8n1 <purohitkrishnar@outlook.com>
2020-09-02 15:30:56 +0530

    Added social media icons and links; made background as full page

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,38 +2,56 @@ import React from 'react';
 import '../css/styles.css';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 import MailIcon from '@material-ui/icons/Mail';
-
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import DescriptionIcon from '@material-ui/icons/Description';
 
 function Body() {
 
     return (
         <div className="App-body">
-            <Grid container spacing={2}>
+            <Grid container spacing={4}>
                 
-                {/* left side containing info */}
-                <Grid item xs={8}>
-                    <Grid container spacing={2} direction="column">
+                {/* empty space of 1 column on the left */}
+                <Grid item xs={1}></Grid>
+
+                <Grid item xs={5}>
+                    <Grid container spacing={3} direction="column">
                         <Grid item xs={6}>
-                            <span className="hello white">んey there @</span>
+                            <span className="hello white">んey there!</span>
                         </Grid>
+
+                        {/* space between header and description */}
+                        <Grid item xs={12} className="empty"></Grid>
+                        
                         <Grid item xs={12}>
                             <span className="text white">
-                                Hello, I'm Krishna Purohit.
+                                Hello, I'm Krishna, I live in Hyderabad, India, and work at Amazon. In my free time I like to program.
+                                
                             </span>
                         </Grid>
                     </Grid>                
                 </Grid>
 
-                {/* right side containing buttons */}
-                <Grid item xs={4}>
+                {/* empty space of 4 columns */}
+                <Grid item xs={4}></Grid>
                 
-                    <Button variant="outlined" className="white-border" href="mailto:purohitkrishnar@gmail.com?subject=Hey There!" >
-                        <MailIcon className="white" />
-                        <span className="text white">Hit me up!</span>
-                    </Button>
+                <Grid item xs={2}>
+                    <a href="mailto:purohitkrishnar@gmail.com?subject=Hey There!" target="_blank">
+                        <MailIcon className="white social-button"></MailIcon>
+                    </a>
+                    <a href="https://twitter.com/purohitkrishnar" target="_blank">
+                        <TwitterIcon className="white social-button"></TwitterIcon>
+                    </a>
+                    <a href="https://www.linkedin.com/in/purohitkrishnar/" target="_blank">
+                        <LinkedInIcon className="white social-button"></LinkedInIcon>
+                    </a>
+                    <a href="https://github.com/16kp" target="_blank">
+                        <GitHubIcon className="white social-button"></GitHubIcon>
+                    </a>
                     
                 </Grid>
 
